fc60b36b403c8b166d50eb2c3429da81bc41a098
Milo MacPhail <milomacphail@gmail.com>
2018-10-10 00:15:42 +0000

    Keydown events pushed to Github

---
+++ script.js
@@ -0,0 +1,8 @@
+/*$(document).ready(function(){
+  $("input").keypress(function(event){
+      ("span").html("Thank you for pressing the letter" + event.which.fromCharCode(); + "! Have a wonderful rest of your day.");
+  });  
+});*/
+
+
+
